767234466bb2ce7bd16065d1618c5d054e726156
virenrb05-upenn <bankapur@seas.upenn.edu>
2026-03-19 21:05:13 -0400

    refactor(03-02): export compute_labels from data package
    - Add compute_labels to lob_forge.data public API
    - Maintain alphabetical import ordering

--- lob_forge/data/__init__.py
+++ lob_forge/data/__init__.py
@@ -1,14 +1,24 @@
 """Data ingestion, preprocessing, and dataset construction for LOB data."""
 
 from lob_forge.data.downloader import BybitDownloader
+from lob_forge.data.features import (
+    compute_all_features,
+    compute_depth_imbalance,
+    compute_microprice,
+    compute_mid_returns,
+    compute_order_imbalance,
+    compute_spread_bps,
+    compute_vpin,
+)
+from lob_forge.data.labels import compute_labels
 from lob_forge.data.lobster import LOBSTERAdapter
-from lob_forge.data.splits import temporal_split
 from lob_forge.data.schema import (
     ALL_COLUMNS,
     LOB_SCHEMA,
     read_lob_parquet,
     write_lob_parquet,
 )
+from lob_forge.data.splits import temporal_split
 from lob_forge.data.validation import compute_quality_metrics, validate_lob_dataframe
 
 __all__ = [
@@ -16,9 +26,17 @@ __all__ = [
     "BybitDownloader",
     "LOB_SCHEMA",
     "LOBSTERAdapter",
+    "compute_all_features",
+    "compute_depth_imbalance",
+    "compute_labels",
+    "compute_microprice",
+    "compute_mid_returns",
+    "compute_order_imbalance",
     "compute_quality_metrics",
+    "compute_spread_bps",
+    "compute_vpin",
     "read_lob_parquet",
-    "validate_lob_dataframe",
     "temporal_split",
+    "validate_lob_dataframe",
     "write_lob_parquet",
 ]
