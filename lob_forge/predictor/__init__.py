1232090efaa6891ccbc2e042a9d7fc885fe275de
virenrb05-upenn <bankapur@seas.upenn.edu>
2026-03-20 14:56:25 -0400

    feat(04-04): update predictor exports and complete config
    - __init__.py re-exports all 6 public classes with __all__
    - predictor.yaml adds features_per_level, n_horizons, max_seq_len

--- lob_forge/predictor/__init__.py
+++ lob_forge/predictor/__init__.py
@@ -1,1 +1,17 @@
 """Spatial-temporal transformer for mid-price movement prediction."""
+
+from lob_forge.predictor.deeplob import DeepLOB
+from lob_forge.predictor.linear_baseline import LinearBaseline
+from lob_forge.predictor.losses import FocalLoss
+from lob_forge.predictor.model import DualAttentionTransformer
+from lob_forge.predictor.spatial_attention import SpatialAttentionBlock
+from lob_forge.predictor.temporal_attention import TemporalAttentionBlock
+
+__all__ = [
+    "DualAttentionTransformer",
+    "DeepLOB",
+    "LinearBaseline",
+    "FocalLoss",
+    "SpatialAttentionBlock",
+    "TemporalAttentionBlock",
+]
